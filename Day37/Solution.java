23ace0ccb570b4512a75b60a4bbe9ce23f2ab382
Mohit Deshmukh <91624758+Mohiitdeshmukh@users.noreply.github.com>
2023-12-31 22:50:24 +0530

    Solution.java

---
+++ Day37/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // Update the first row
+        for (int i = 1; i < cols; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+
+        // Update the first column
+        for (int i = 1; i < rows; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        // Calculate minimum path sum for each cell
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[rows - 1][cols - 1];
+    }
+}
